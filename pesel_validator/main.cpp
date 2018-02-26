ea1cd4dad12309ce6b6abe33d856ab9912f48463
Adam Stanisławski <poncki1643@gmail.com>
2018-02-26 20:29:18 +0100

    Initial Commit

---
+++ pesel_validator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pesel_validator
+//
+//  Created by Adam Stanislawski on 26.02.2018.
+//  Copyright © 2018 Adam Stanislawski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
